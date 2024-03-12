40382885346a7bfa4bbc908e78b2859095ff9f91
Ali <Ali@DESKTOP-1CTC3D9>
2024-03-12 10:51:03 -0400

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace DungeonCrawler
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+            
+
+            
+            StartBtn.Text = $"Game Starting";
+            
+            ////SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+
+
+    }
+
+    }
